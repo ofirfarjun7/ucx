dbd60b8c01ea02816f1d5ef098ba5a7c6436917c
Ofir Farjon <ofarjon@nvidia.com>
2022-08-08 13:55:32 +0300

    UCT/AM: Change UCT AM handlers API to support additional optional parameters

--- src/uct/base/uct_iface.h
+++ src/uct/base/uct_iface.h
@@ -875,13 +875,14 @@ int uct_iface_local_is_reachable(uct_iface_local_addr_ns_t *addr_ns,
  *
  * @param iface    Interface to invoke the handler for.
  * @param id       Active message ID.
- * @param data     Received data.
+ * @param msg_hdr  Received msg_hdr.
  * @param length   Length of received data.
  * @param flags    Mask with @ref uct_cb_param_flags
  */
 static inline ucs_status_t
-uct_iface_invoke_am(uct_base_iface_t *iface, uint8_t id, void *data,
-                    void *payload, unsigned length, unsigned flags)
+uct_iface_invoke_am(uct_base_iface_t *iface, uint8_t id, void *msg_hdr,
+                    unsigned length, unsigned flags,
+                    uct_am_callback_params_t *params)
 {
     ucs_status_t     status;
     uct_am_handler_t *handler;
@@ -893,13 +894,13 @@ uct_iface_invoke_am(uct_base_iface_t *iface, uint8_t id, void *data,
     UCS_STATS_UPDATE_COUNTER(iface->stats, UCT_IFACE_STAT_RX_AM_BYTES, length);
 
     handler = &iface->am[id];
-    status  = handler->cb(handler->arg, data, payload, length, flags);
+    status  = handler->cb(handler->arg, msg_hdr, length, flags, params);
     ucs_assertv((status == UCS_OK) ||
                 ((status == UCS_INPROGRESS) && (flags &
                                                 UCT_CB_PARAM_FLAG_DESC)),
-                "%s(arg=%p data=%p length=%u flags=0x%x) returned %s",
+                "%s(arg=%p msg_hdr=%p length=%u flags=0x%x) returned %s",
                 ucs_debug_get_symbol_name((void*)handler->cb), handler->arg,
-                data, length, flags, ucs_status_string(status));
+                msg_hdr, length, flags, ucs_status_string(status));
     return status;
 }
 
