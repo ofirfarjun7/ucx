0eac0058c1da48b84b9ac03f24631162aa8bce39
Ofir Farjon <ofarjon@nvidia.com>
2025-05-29 20:39:35 +0000

    Use GDRcopy conditionally.

--- src/tools/perf/gda-ki/gdaki_mem_handle.cu
+++ src/tools/perf/gda-ki/gdaki_mem_handle.cu
@@ -1,24 +1,31 @@
 #include "gdaki_mem_handle.h"
-#include <gdrapi.h>
 #include <cuda_runtime.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#ifdef HAVE_GDR_COPY
+#include <gdrapi.h>
+#endif
+
 #define PAGE_ROUND_UP(x, n)     (((x) + ((n) - 1)) & ~((n) - 1))
 
 struct gdaki_mem_handle {
     void*    gpu_ptr;
     void*    cpu_ptr;
     size_t   size;
+#ifdef HAVE_GDR_COPY
     gdr_t    gdr;
     gdr_mh_t mh;
-    bool     owns_gpu_mem;  // True if we allocated the GPU memory
+#endif
+    bool     owns_gpu_mem;
 };
 
 gdaki_mem_handle_t gdaki_mem_create(void* gpu_ptr, size_t size) {
+#ifndef HAVE_GDR_COPY
+    return NULL;
+#else
     gdaki_mem_handle_t handle = (gdaki_mem_handle_t)calloc(1, sizeof(*handle));
-    int ret;
-
     if (!handle) {
         return NULL;
     }
@@ -31,14 +38,17 @@ gdaki_mem_handle_t gdaki_mem_create(void* gpu_ptr, size_t size) {
         if (cuda_err != cudaSuccess) {
             fprintf(stderr, "Failed to allocate GPU memory: %s\n", 
                     cudaGetErrorString(cuda_err));
-            goto free;
+            free(handle);
+            return NULL;
         }
         handle->owns_gpu_mem = true;
         cuda_err = cudaMemset(handle->gpu_ptr, 0, handle->size);
         if (cuda_err != cudaSuccess) {
             fprintf(stderr, "Failed to initialize GPU memory: %s\n", 
                     cudaGetErrorString(cuda_err));
-            goto free_cuda;
+            cudaFree(handle->gpu_ptr);
+            free(handle);
+            return NULL;
         }
     } else {
         assert(size % GPU_PAGE_SIZE == 0);
@@ -50,46 +60,55 @@ gdaki_mem_handle_t gdaki_mem_create(void* gpu_ptr, size_t size) {
     handle->gdr = gdr_open();
     if (!handle->gdr) {
         fprintf(stderr, "GDRcopy initialization failed\n");
-        goto free_cuda;
+        goto cleanup;
     }
 
     // Pin and map the buffer
-    ret = gdr_pin_buffer(handle->gdr, (unsigned long)handle->gpu_ptr, handle->size, 0, 0, &handle->mh);
+    int ret = gdr_pin_buffer(handle->gdr, (unsigned long)handle->gpu_ptr, 
+                            handle->size, 0, 0, &handle->mh);
     if (ret) {
         fprintf(stderr, "GDRcopy pin buffer failed\n");
-        goto close;
+        gdr_close(handle->gdr);
+        goto cleanup;
     }
 
     ret = gdr_map(handle->gdr, handle->mh, &handle->cpu_ptr, handle->size);
     if (ret) {
         fprintf(stderr, "GDRcopy map failed\n");
-        goto unpin;
+        gdr_unpin_buffer(handle->gdr, handle->mh);
+        gdr_close(handle->gdr);
+        goto cleanup;
     }
 
     return handle;
 
-unpin:
-    gdr_unpin_buffer(handle->gdr, handle->mh);
-close:
-    gdr_close(handle->gdr);
-free_cuda:
+cleanup:
     if (handle->owns_gpu_mem) {
         cudaFree(handle->gpu_ptr);
     }
-free:
     free(handle);
     return NULL;
+#endif
 }
 
 void* gdaki_mem_get_ptr(gdaki_mem_handle_t handle) {
+#ifndef HAVE_GDR_COPY
+    return NULL;
+#else
     return handle ? handle->cpu_ptr : NULL;
+#endif
 }
 
 void* gdaki_mem_get_gpu_ptr(gdaki_mem_handle_t handle) {
+#ifndef HAVE_GDR_COPY
+    return NULL;
+#else
     return handle ? handle->gpu_ptr : NULL;
+#endif
 }
 
 void gdaki_mem_destroy(gdaki_mem_handle_t handle) {
+#ifdef HAVE_GDR_COPY
     if (!handle) {
         return;
     }
@@ -103,4 +122,5 @@ void gdaki_mem_destroy(gdaki_mem_handle_t handle) {
     }
     
     free(handle);
+#endif
 } 
